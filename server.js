c71e8ffe6605b086b323cdbacd2269e4753c3c64
Shiho Asa <shiasa@microsoft.com>
2019-02-02 17:11:30 +0900

    Update: serviceA

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('We love Software Design!!!\n');
+  res.send('We love Software Design!\n');
 });
 
 app.listen(PORT, HOST);
